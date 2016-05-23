b7d9c803c03043b430609da5d7f55922ced32ab6
NathanOrme <nathan.orme23@gmail.com>
2016-05-23 17:22:44 +0100

    reformated persist test for supplierController

--- test/SupplierControllerTest.java
+++ test/SupplierControllerTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.qa.ims.App;
 import com.qa.ims.controller.SupplierController;
 import com.qa.ims.model.SupplierModel;
+import com.qa.ims.model.repository.SupplierModelRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = App.class)
@@ -17,6 +20,8 @@ public class SupplierControllerTest {
 	
 	@Autowired
 	private SupplierController supplierController;
+	@Autowired
+	private SupplierModelRepository supplierRepository;
 	private SupplierModel supplierModel;
 	
 	@Before
@@ -26,7 +31,9 @@ public class SupplierControllerTest {
 
 	@Test
 	public void testPersistSupplier() {
-		assertSame(supplierController.persistSupplier(supplierModel), "Added Supplier");
+		supplierController.persistSupplier(supplierModel);
+		List<SupplierModel> sm = supplierRepository.findAll();
+		assertNotEquals(sm, null);
 	}
 
 	@Test
