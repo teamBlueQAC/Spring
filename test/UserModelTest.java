14f18aa0845630364dbcba40c877227f63de54e6
willseaford <willseaford@gmail.com>
2016-05-23 15:12:47 +0100

    actual testing folder -- for realz this time

---
+++ test/UserModelTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserModelTest {
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+}
