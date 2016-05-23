a5602dac95fe7375bcf5360fe8b5cb4a7787cde2
willseaford <willseaford@gmail.com>
2016-05-23 15:31:57 +0100

    edited pom file to include JUnit dependency

---
+++ src/main/java/com/qa/ims/Order.java
@@ -0,0 +1,13 @@
+package com.qa.ims;
+
+import java.util.Date;
+
+public class Order {
+	
+	private long id;
+	private double total;
+	private Date date;
+	private Date dispatchDate;
+	//private OrderStatus orderStatus;
+
+}
